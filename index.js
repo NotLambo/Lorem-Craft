4bd821e6fca81bfe028299b4e6e22ec7b38e5d35
Lambo <100071045+NotLambo@users.noreply.github.com>
2023-07-27 15:53:53 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,98 +1,58 @@
+'use strict';
 // Main Variables
-const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Porttitor eget dolor morbi non arcu risus quis. Commodo quis imperdiet massa tincidunt. Feugiat sed lectus vestibulum mattis ullamcorper. Neque viverra justo nec ultrices dui sapien eget mi. Dui faucibus in ornare quam viverra. Euismod in pellentesque massa placerat duis ultricies lacus. Viverra tellus in hac habitasse platea dictumst vestibulum rhoncus. Nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Lorem ipsum dolor sit amet consectetur adipiscing elit. Mauris in aliquam sem fringilla ut morbi tincidunt augue. Magna ac placerat vestibulum lectus mauris ultrices eros in. Non nisi est sit amet facilisis magna etiam tempor orci. Ornare aenean euismod elementum nisi. Suscipit adipiscing bibendum est ultricies integer quis auctor elit. At tellus at urna condimentum.`;
-const output = document.querySelector('.output');
-const creditLogo = document.querySelector('.-logo');
+const outputText = document.querySelector('.result');
 
 // Buttons
-const menu = document.querySelector('.menu');
-const userConfirmation = document.querySelector('.user-confirm');
-const settingsBtn = document.querySelector('.settings-btn');
-const lightDarkBtn = document.querySelector('.light-dark');
-const exitBtn = document.querySelectorAll('.exit-button');
-const copyBtn = document.querySelector('.copy');
+const userConfirm = document.querySelector('.userConfirm');
+const closeUpdateWindow = document.querySelector('.close-update');
+const copyButton = document.querySelector('.copy');
+const options = document.querySelector('.generateOptions');
 
-// Divs and areas
-const generateArea = document.querySelector('.generate-area');
-const dropDown = document.querySelector('.drop-main');
-const settingsMenu = document.querySelector('.settings-main');
-const copiedDiv = document.querySelector('.copy-main');
-Array.from(document.querySelector('.dropdown'));
+// Divs/Areas
+const updateWindow = document.querySelector('.modal-dialog');
+const output = document.querySelector('.main-out');
+const alert = document.querySelector('.alert');
 
-// Events
+let result;
 
-userConfirmation.addEventListener('click', function() {
-  const userInput = document.querySelector('.user-in').value;
-  const result = lorem.repeat(userInput);
-  output.textContent = `${result}`;
-  creditLogo.classList.remove('hidden');
-  copyBtn.addEventListener('click', function() {
-    navigator.clipboard.writeText(result); // copies the Lorem result too users clipboard.
-    copiedDiv.classList.remove('hidden');
-    copyBtn.src = `Assets/copying.png`;
-    setTimeout(() => {
-      copyBtn.src = `Assets/copy.png`;
-      copiedDiv.classList.add('hidden');
-    }, 5000)
-  })
+// Event Listeners
+userConfirm.addEventListener('click', function() {
+  output.classList.remove('hidden');
+  const userChoice = document.querySelector('.generateOptions').value;
+  const userInput = document.querySelector('.userInput').value;
+  if(userChoice === `paragraphGeneration`) {
+    result = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ipsa beatae, eligendi blanditiis in quae officiis? Placeat perferendis suscipit qui, minima eligendi repellendus quasi quibusdam nam vitae beatae, sed nemo.`.repeat(userInput);
+    outputText.textContent = result;
+  }
+  if(userChoice === `sentenceGeneration`) {
+    result = `Lorem ipsum dolor sit amet consectetur adipisicing elit.`.repeat(userInput);
+    outputText.textContent = result;
+  }
+  if(userChoice === `wordGeneration`) {
+    const words = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet'];
+    const wordGeneration = words[Math.trunc(Math.random() * words.length)];
+    result = `${wordGeneration} `.repeat(userInput);
+    outputText.textContent = result;
+  }
 })
 
-menu.addEventListener('click', function() {
-  closeAll();
-  openMenu();
-  settings();
-  exitButton();
+
+copyButton.addEventListener('click', function () {
+  alertBox();
 })
+
+closeUpdateWindow.addEventListener('click', () => {updateWindow.classList.add('close-modal'), setTimeout(() => {document.querySelector('.modal-sheet').classList.remove('d-block')}, 200)});
 
 // Functions
 
-function openMain() {
-  closeAll();
-  generateArea.classList.remove('hidden');
-}
-
-function closeAll() {
-  generateArea.classList.add('hidden');
-  dropDown.classList.add('hidden');
-  settingsMenu.classList.add('hidden');
-}
-
-function openMenu() {
-  dropDown.classList.remove('hidden');
-}
-
-function settings() {
-  settingsBtn.addEventListener('click', function() {
-    closeAll();
-    settingsMenu.classList.remove('hidden');
-    lightDark();
-  });
-}
-
-function styleSwitch(style, color) {
-  lightDarkBtn.textContent = `${style}`;
-  document.querySelector(':root').style.backgroundColor = `${color}`;
-}
-
-function lightDark() {
-  lightDarkBtn.addEventListener('click', function() {
-    const x = lightDarkBtn.textContent;
-    if(x.includes('Light Mode.')) {
-      styleSwitch(`Dark Mode.`, `#474E68`);
-      lightDarkBtn.style.backgroundColor = '#181617';
-    } else {
-      styleSwitch(`Light Mode.`, `#181617`);
-      lightDarkBtn.style.backgroundColor = '#474E68';
-    }
-  })
-}
-
-function exitButton() {
-  Array.from(exitBtn);
-  for(const btn of exitBtn) {
-    btn.addEventListener('click', function() {
-      openMain();
-    })
-  }
+function alertBox() {
+  navigator.clipboard.writeText(result);
+  alert.classList.remove('hidden');
+  copyButton.src = `Assets/copying-min.png`;
+  setTimeout(() => {
+    alert.classList.add('hidden');
+    copyButton.src = `Assets/copy-min.png`;
+  }, 5000);
 }
 
 /*
